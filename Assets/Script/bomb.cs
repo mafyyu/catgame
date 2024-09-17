aea54a3948bd8efe25d4e199810edb338e5d90fc
sstt126 <g2453828@cc.kyoto-su.ac.jp>
2024-09-17 14:44:49 +0900

    爆弾の追加
    爆弾の追加

---
+++ Assets/Script/bomb.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bomb : MonoBehaviour
+{
+    [SerializeField, Header("爆発エフェクト")]
+    public GameObject explosionEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D (Collision2D col)
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.5f);
+        foreach (Collider2D collider in colliders)
+        {
+            if (collider.tag == "Enemy")
+            {
+                Destroy(collider.gameObject);
+            }
+            Instantiate(explosionEffect, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+        
+    }
+}
