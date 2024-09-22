36a477096251dc8b60ff2410f61c6adfabc58a2b
sstt126 <g2453828@cc.kyoto-su.ac.jp>
2024-09-23 00:30:47 +0900

    ステージの作成、爆弾を増やすアイテム追加

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -11,23 +11,36 @@ public class Enemy : MonoBehaviour
     private int _attackPower; 
 
     private Rigidbody2D _rigid;
+    private Camera _mainCamera;
 
     // Start is called before the first frame update
     void Start()
     {
         _rigid = GetComponent<Rigidbody2D>();
+        _mainCamera = Camera.main; // メインカメラの参照を取得
     }
 
     // Update is called once per frame
     void Update()
     {
-        _Move();
+        if (_IsVisibleFromCamera())
+        {
+            _Move();
+        }
     }
 
     private void _Move()
     {
-        _rigid.velocity = new Vector2(Vector2.left.x*_moveSpeed , _rigid.velocity.y);
+        _rigid.velocity = new Vector2(Vector2.left.x * _moveSpeed, _rigid.velocity.y);
         //敵が左方向へ動くようにする
+    }
+
+    private bool _IsVisibleFromCamera()
+    {
+        // 敵の位置をスクリーン座標に変換
+        Vector3 screenPoint = _mainCamera.WorldToViewportPoint(transform.position);
+        // スクリーン座標がカメラの視界内にあるかどうかをチェック
+        return screenPoint.x > 0 && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
     }
 
     public void PlayerDamage(Player player) //ダメージを与える処理
