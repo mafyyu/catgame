3405f21befa0710b8ab6c607698ea13e1a29c2b1
sstt126 <g2453828@cc.kyoto-su.ac.jp>
2024-10-05 00:23:02 +0900

    敵の追加（かえる）

--- Assets/Script/Frog.cs
+++ Assets/Script/Frog.cs
@@ -2,19 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Flog : MonoBehaviour
 {
     [SerializeField, Header("移動速度")]
-    private float _moveSpeed;
+    private float _moveSpeed; 
+
+    [SerializeField, Header("ジャンプ速度")]
+    private float _jumpSpeed;
 
     private Rigidbody2D _rigid;
     private Camera _mainCamera;
+    private Animator _anim;
 
     // Start is called before the first frame update
     void Start()
     {
         _rigid = GetComponent<Rigidbody2D>();
         _mainCamera = Camera.main; // メインカメラの参照を取得
+        _anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -38,5 +43,23 @@ public class Enemy : MonoBehaviour
         Vector3 screenPoint = _mainCamera.WorldToViewportPoint(transform.position);
         // スクリーン座標がカメラの視界内にあるかどうかをチェック
         return screenPoint.x > 0 && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Floor" || collision.gameObject.tag == "Block")
+        {
+                _rigid.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
+                _anim.SetBool("Jump", false);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Floor" || collision.gameObject.tag == "Block")
+        {
+            _anim.SetBool("Jump", true);
+        }
     }
 }
